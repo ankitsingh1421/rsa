5adde749b6074aefc60eb5b47eec8955d022819a
ankitsingh1421 <ankitsingh75107@gmail.com>
2025-05-31 21:21:13 +0530

    addn new files

--- backend/routes/confirmationRoutes.js
+++ backend/routes/confirmationRoutes.js
@@ -1,7 +1,18 @@
 import express from 'express';
-import { createConfirmation } from '../controllers/confirmationController.js';
+import {
+  createConfirmation,
+  getAllConfirmations,
+  getConfirmationById,
+  updateConfirmation,
+  deleteConfirmation
+} from '../controllers/confirmationController.js';
+
 const router = express.Router();
 
 router.post('/create', createConfirmation);
+router.get('/', getAllConfirmations); // Get all
+router.get('/:id', getConfirmationById); // Get by ID
+router.put('/:id', updateConfirmation); // Edit/Update
+router.delete('/:id', deleteConfirmation); // Delete
 
 export default router;
